1ad90592586ddae015463f4e0af5ddcd6f91142b
AJ Boyd <aj.boyd2015@gmail.com>
2025-03-01 17:54:18 -0700

    Data Object Classes created in model package

---
+++ shared/src/main/java/model/GameData.java
@@ -0,0 +1,12 @@
+package model;
+
+import chess.ChessGame;
+
+public record GameData(int gameID, String whiteUsername, String blackUsername, String gameName, ChessGame game) {
+    public GameData setWhitePlayer(String whitePlayer){
+        return new GameData(gameID, whitePlayer, blackUsername, gameName, game);
+    }
+    public GameData setBlackPlayer(String blackPlayer){
+        return new GameData(gameID, whiteUsername, blackPlayer, gameName, game);
+    }
+}

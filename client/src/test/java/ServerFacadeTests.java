fff8910d02e08f22fe8c7aa16cab3138116953c7
AJ Boyd <aj.boyd2015@gmail.com>
2025-03-25 10:39:40 -0600

    Register facade tests passing

--- client/src/test/java/ServerFacadeTests.java
+++ client/src/test/java/ServerFacadeTests.java
@@ -1,16 +1,25 @@
+import dataaccess.DataAccessException;
+import model.AuthData;
+import model.UserData;
 import org.junit.jupiter.api.*;
 import server.Server;
+import service.ClearService;
+
+import java.lang.reflect.Field;
 
 
 public class ServerFacadeTests {
 
     private static Server server;
+    private static String URL;
+    private static ServerFacade facade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+        URL = "http://localhost:" + port;
     }
 
     @AfterAll
@@ -18,10 +27,34 @@ public class ServerFacadeTests {
         server.stop();
     }
 
+    @BeforeEach
+    public void setUp(){
+        facade = new ServerFacade(URL);
+    }
+
+    @AfterEach
+    public void cleanUp() throws NoSuchFieldException, IllegalAccessException, DataAccessException {
+        //reset server contents
+        Field clearService = server.getClass().getDeclaredField("CLEAR_SERVICE");
+        clearService.setAccessible(true);
+        ((ClearService) clearService.get(server)).clearAll();
+    }
 
     @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
+    public void registerSuccess() {
+        UserData user = new UserData("Jethro", "password", "example@aol.com");
+        Assertions.assertDoesNotThrow(() -> {
+            AuthData response = facade.register(user);
+            assert response != null;
+        });
+    }
+
+    @Test
+    public void registerFail() {
+        UserData user = new UserData("Jethro", "password", "example@aol.com");
+
+        Assertions.assertDoesNotThrow(() -> facade.register(user));
+        Assertions.assertThrows(ResponseException.class, () -> facade.register(user));
     }
 
 }
